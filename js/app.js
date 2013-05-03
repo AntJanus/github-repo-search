f279f8627f408c2add85c4b9d9eed074b410f0d7
Antonin Januska <antonin@antjanus.com>
2013-05-03 00:07:49 -0500

    jquery sucks

--- js/app.js
+++ js/app.js
@@ -4,19 +4,14 @@ angular.module('gitApp', []).filter('fromNow', function() {
   }
 });
 
-function mainCtrl($scope){
+function mainCtrl($scope, $http){
 	//load github
 	//execute upon hitting "search"
 	$scope.loadGit = function(username){
-		$.ajax({
-			type:'GET',
-			url: 'https://api.github.com/users/'+username+'/repos',
-			success: function(data){
+		var url = 'https://api.github.com/users/'+username+'/repos';
+		$http({method: 'get', url: url}).success(function(data){
 				$scope.repos = data;
 				$scope.username = username;
-				//this is what happens when you use jQuery kids, be careful
-				$scope.$apply();
-			}
 		});
 	}
 	//default empty (no pre-execution)
